94506f0589197a8fcc818c10141f7f1e0436b351
Johan Millers <johanoscarmarte@gmail.com>
2021-01-24 12:12:18 -0400

    Validacion de yargs

---
+++ config/yargs.js
@@ -0,0 +1,28 @@
+const argv = require('yargs')
+                 .command('crear','crear un elemento por hacer',{
+    descripcion :{
+        demand: true,
+        alias: 'd',
+        desc: 'Descripcion de la tarea por hacer'
+    }
+})
+.command('actualizar','Actualiza el estado completado de una tarea',{
+    descripcion : {
+        demand: true,
+        alias: 'd',
+        desc: 'Descripcion de la tarea por hacer'
+
+    },
+    completado : {
+        alias: 'c',
+        default: true,
+        desc: 'Marca como completado o pendiente la tarea'
+    }
+})
+.help()
+.argv;
+
+
+module.exports = {
+    argv
+}
