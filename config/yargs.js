07d0e7a4c701bc826e48971ce2839ac1f673be8a
Johan Millers <johanoscarmarte@gmail.com>
2021-01-24 17:12:40 -0400

    Optimizacion de los comando

--- config/yargs.js
+++ config/yargs.js
@@ -1,30 +1,26 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+
+};
+
 const argv = require('yargs')
                  .command('crear','crear un elemento por hacer',{
-    descripcion :{
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea por hacer'
-    }
+    descripcion
 })
 .command('actualizar','Actualiza el estado completado de una tarea',{
-    descripcion : {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea por hacer'
-
-    },
-    completado : {
-        alias: 'c',
-        default: true,
-        desc: 'Marca como completado o pendiente la tarea'
-    }
+    descripcion,
+    completado
 })
 .command('borrar','borrar un tarea que ya no necesitamos', {
-    descripcion: {
-        demand: true,
-        alias: 'd',
-        desc: "Borrar la tarea selecionado"
-    }
+    descripcion
 })
 .help()
 .argv;
