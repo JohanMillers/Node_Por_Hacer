aceb2e8ac1d85d98b5bf774145c986c744d39a43
Johan Millers <johanoscarmarte@gmail.com>
2021-01-24 14:01:27 -0400

    Creando una tarea

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const argv = require('./config/yargs').argv;
 
+const porHacer = require('./porHacer/porHacer');
+
+
 let comando =  argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
     break;
     case 'listar':
         console.log('Mostar todas las tarea por hacer');
